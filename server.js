2cdf66ff83ebade38ba6329bf5570879e3f3a1ce
coledtaylor <coledtaylor3@gmail.com>
2021-02-18 18:24:35 -0700

    added public folder I forgot

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 
 const app = express()
 
+// sets view engine to ejs
 app.set('view engine', 'ejs')
 
+// Serves static assets from public folder
+app.use(express.static(__dirname + 'public'));
+
+// Allows app to parse json data
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+// Basic index route
 app.get('/', (req, res) =>{
   res.render('index', {name: 'BCSC'})
 })
 
-
+// Server listens on port 5000
 const PORT = process.env.PORT || 5000
 const HOST = process.env.HOST
 
